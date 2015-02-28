e5f8ebefc45fc909daf2e247cb13b38bc5fa1fcb
milo2012 <root@localhost.localdomain>
2015-02-28 06:24:20 -0500

    fix a bunch of bugs and added nocache option

--- metasploitHelper.py
+++ metasploitHelper.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*- 
-#!/usr/bin/python
+# -*- coding: utf-8 -*- !/usr/bin/python
 
 #pip install python-libnmap
 
@@ -11,7 +10,9 @@ import argparse
 import sys
 import requests
 import multiprocessing
+import commands
 
+path = "/pentest/metasploit-framework/modules/"
 requests.packages.urllib3.disable_warnings()
 
 uriList=[]
@@ -22,6 +23,8 @@ portMatch=[]
 contentList=[]
 findWeb=True
 findPort=True
+
+contentList.append("spool runMsf.log")
 
 class Worker1(multiprocessing.Process):
 
@@ -61,6 +64,20 @@ def execute1(jobs, num_processes=2):
     results.sort()
     return (results)
 
+def RunCommand(fullCmd):
+    try:
+        return commands.getoutput(fullCmd)
+    except:
+        return "Error executing command %s" %(fullCmd)
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
 def getHEAD(scheme,hostNo,uri,portNo):
 	url = scheme+"://"+hostNo+":"+portNo+uri
 	print url
@@ -74,6 +91,146 @@ def getHEAD(scheme,hostNo,uri,portNo):
 	except Exception as e:
 		print e
 		pass
+
+def lookupAllPorts():
+	fullCmd = 'grep -ir "Opt::RPORT" '+path
+	results =  RunCommand(fullCmd)
+	writeList=[]
+	portList=[]
+	lookupList=[]
+	exploitList=[]
+	resultsList = results.split("\n")
+	for result in resultsList:
+		result1 = result.split(".rb:")[1].strip()
+		exploitModule = result.split(".rb:")[0].strip()+".rb"
+		portNo = find_between(result1,"RPORT(",")")
+		portNo = portNo.replace("'","")
+		if not any(c.isalpha() for c in portNo):
+			if "fuzzer" not in exploitModule and "auxiliary/dos" not in exploitModule:
+				exploitList.append([exploitModule,str(portNo)])
+	for x in exploitList:
+		lines=[]
+		filename = x[0]
+		matchPort = x[1]
+		if "fuzzer" not in filename:
+			moduleName = filename.replace(path,"")
+
+			tempStrList=[]
+			finalList=[]
+			with open(filename) as f:
+				lines = f.read().splitlines()
+			startFound=False
+			optionList=[]
+			found=False
+			for line in lines:
+				if "register_options" in line:
+					startFound=True
+				if "self.class)" in line and found==False:
+					found1=False
+					for y in optionList:
+						if found1==True:
+							y = y.strip()
+							if "#" not in y:
+								tempStrList.append(y)
+						if ".new" in y:
+							if found1==True:
+								tempStrList=[]
+								found1=False
+							if "[" in y and "]" in y:
+								y = y.strip()
+								finalList.append(y)
+							else:
+								y = y.strip()
+								if "#" not in y:
+									tempStrList.append(y)
+									found1=True
+					startFound=False
+					found=True
+				if startFound==True:
+					optionList.append(line)
+			result1=""
+			for y in tempStrList:
+				try:	
+					m = re.search('"(.+?)"',y)
+					temp1 = str(m.group(1)).replace(",","")
+					y = y.replace(m.group(1),temp1)
+					result1 += y
+				except AttributeError:
+					result1 += y
+					continue
+			if len(str(result1))>0:
+				result1 = result1.replace(" ","")
+				finalList.append(result1)
+			tempStr1=""
+			for g in finalList:
+				if "false" not in g.lower() and "rhost" not in g.lower():
+					parameterList = g.partition('[')[-1].rpartition(']')[0]
+					parNameTemp =( g.split(",")[0]).partition("'")[-1].rpartition("'")[0]
+					result = (parameterList.split(",")[-1]).strip()
+					if result=='""' or result=="''":
+						tempStr1+= parNameTemp
+						tempStr1+= ","
+			moduleName = moduleName.replace(".rb","")
+			if len(tempStr1)>0:
+				if tempStr1[-1]==",":
+					results = matchPort+","+moduleName+",["+tempStr1[0:(len(tempStr1)-1)]+"]"
+					writeList.append(results)
+				else:
+					results = matchPort+","+moduleName+",["+tempStr1[0:(len(tempStr1)-1)]+"]"
+					writeList.append(results)
+			else:
+				results = matchPort+","+moduleName+",[]"
+				writeList.append(results)
+	f = open('port2Msf.csv','w')
+	for x in writeList:
+		f.write(x+"\n")
+	f.close()
+
+def lookupURI(showModules=False):
+	fullCmd = 'grep -ir "OptString.new(\'TARGETURI\'" '+path
+	results =  RunCommand(fullCmd)
+	exploitList=[]
+	pathList=[]
+	uriList=[]
+	defaultPathList=[]
+	resultsList = results.split("\n")
+	for result in resultsList:
+		result1 = result.split(".rb:")[1].strip()
+		exploitModule = result.split(".rb:")[0].strip()
+		exploitModule = exploitModule.replace(path,"")
+	
+		portNo = find_between(result1,"[","]")
+		if "fuzzer" not in exploitModule and "auxiliary/dos" not in exploitModule:
+			exploitList.append([exploitModule,portNo])
+			result1 = portNo.split(",")[-1]
+			result1 = result1.replace("'","")
+			result1 = result1.replace('"',"")
+			result1 = result1.strip()
+			if "/" in result1:
+				exploitModule = exploitModule.replace(".rb","")
+				if result1!="/":
+					#if not result1.endswith("/"):
+					#	result1 = result1+"/"
+					if result1 not in uriList:
+						uriList.append(result1)
+				if result1=="/":
+					defaultPathList.append([result1,exploitModule])
+				else:
+					if result1 not in pathList:		
+						pathList.append([result1,exploitModule])
+	f = open('uriList.txt','w')
+	for x in uriList:
+		f.write(x+"\n")
+	f.close()
+	
+	f = open('default-path.csv','w')
+	for x in pathList:
+		x[1] = x[1].replace(path,".")
+		f.write( x[0]+","+x[1]+"\n")
+	for x in defaultPathList:	
+		x[1] = x[1].replace(path,".")
+		f.write( x[0]+","+x[1]+"\n")
+	f.close()
 
 def readDatabase():
 	fname="default-path.csv"
@@ -102,10 +259,10 @@ def lookupPort(hostNo,portNo):
 	for y in portMatch:	
 		#print y[0]+"\t"+portNo
 		if y[0]==portNo:
-			contentList.append("USE "+y[1])
-			contentList.append("SET RHOST "+hostNo)
-			contentList.append("SET RHOSTS "+hostNo)
-			contentList.append("SET RPORT "+portNo)
+			contentList.append("use "+y[1])
+			contentList.append("set RHOST "+hostNo)
+			contentList.append("set RHOSTS "+hostNo)
+			contentList.append("set RPORT "+portNo)
 			contentList.append("exploit\n")
 
 def testURI(scheme,hostNo,portNo):
@@ -140,62 +297,49 @@ def testURI(scheme,hostNo,portNo):
 					print x[1]
 					o = urlparse(x[1])
 					contentList.append('use '+msfModule)
-					contentList.append('SET RHOST '+(o.netloc).split(":")[0])
-					contentList.append('SET RHOSTS '+(o.netloc).split(":")[0])
-					contentList.append('SET RPORT '+(o.netloc).split(":")[1])
+					contentList.append('set RHOST '+(o.netloc).split(":")[0])
+					contentList.append('set RHOSTS '+(o.netloc).split(":")[0])
+					contentList.append('set RPORT '+(o.netloc).split(":")[1])
 					contentList.append('exploit')
 def parseNmap(filename):
 	ipList=[]
 	httpList=[]
 	httpsList=[]
 	portList=[]
+	portsList=[]
+	stateList=[]
+	serviceList=[]
 
 	with open (filename, 'rt') as file:
 		tree=ElementTree.parse(file)
+	for node_2 in tree.iter('state'):
+		state =   node_2.attrib.get('state')
+		stateList.append(state)
+	for node_2 in tree.iter('port'):
+		port =   node_2.attrib.get('portid')
+		portList.append(port)
 	for node_2 in tree.iter('address'):
 		ip_add =   node_2.attrib.get('addr')
-		#print ip_add
 		ipList.append(ip_add)
+	for node_2 in tree.iter('service'):
+		service =   node_2.attrib.get('name')
+		serviceList.append(service)
 	rep = NmapParser.parse_fromfile(filename) 
 	for _host in rep.hosts:
 		host = ', '.join(_host.hostnames)
 		ip = (_host.address)
-	#	print "----------------------------------------------------------------------------- "
-	#	print "HostName: "'{0: >35}'.format(host,"--", ip)
-	#Lists in order to store Additional information, Product and version next to the port information.
-	list_name=[]
-	list_tunnel=[]
-	list_product=[]
-	list_version=[]
-	list_extrainf=[]
-	for node_4 in tree.iter('service'): #ElementTree manipulation. Service Element which included the sub-elements product, version, extrainfo
-		name = node_4.attrib.get('name')
-		tunnel = node_4.attrib.get('tunnel')
-		product = node_4.attrib.get('product')
-		version = node_4.attrib.get('version')
-		extrainf = node_4.attrib.get('extrainfo')
-		list_name.append(name)
-		list_tunnel.append(tunnel)
-		list_product.append(product)
-		list_version.append(version)
-		list_extrainf.append(extrainf)
-	for osmatch in _host.os.osmatches: #NmapParser manipulation to detect OS and accuracy of detection.
-		os = osmatch.name
-		accuracy = osmatch.accuracy
-		#print "Operating System Guess: ", os, "- Accuracy Detection", accuracy
-		break
-		print "----------------------------------------------------------------------------- "
-	counter = 0
-	for services in _host.services: #NmapParser manipulation to list services, their ports and their state. The list elements defined above are printed next to each line.
-		if list_name[counter]=="http":
-			httpList.append([ipList[counter],str(services.port)])
-			#print list_name[counter]
-			#print ipList[counter]+"\t"+str(services.port)+"\t"+services.state+"\t"+list_product[counter]+"\t"+list_version[counter]
-		elif list_name[counter]=='https':
-			httpsList.append([ipList[counter],str(services.port)])
-		else:
-			portList.append([ipList[counter],str(services.port)])
-		counter = counter + 1
+
+	counter=0
+	while counter<len(stateList):
+		if stateList[counter]=="open":
+			if serviceList[counter]=="http":
+				httpList.append([ipList[counter],portList[counter]])
+			elif serviceList[counter]=="https":
+				httpsList.append([ipList[counter],portList[counter]])
+			else:
+				portsList.append([ipList[counter],portList[counter]])
+		counter+=1
+
 	if findWeb==True:
 		if len(httpList)>0:
 			for x in httpList:
@@ -205,7 +349,6 @@ def parseNmap(filename):
 				portNo = x[1]
 				print url
 				testURI(scheme,hostNo,portNo)
-
 	if findWeb==True:
 		if len(httpsList)>0:
 			for x in httpsList:
@@ -216,16 +359,16 @@ def parseNmap(filename):
 				print url
 				testURI(scheme,hostNo,portNo)
 	if findPort==True:
-		if len(portList)>0:
-			for x in portList:
+		if len(portsList)>0:
+			for x in portsList:
 				lookupPort(x[0],x[1])
 if __name__== '__main__':
     parser= argparse.ArgumentParser()
     parser.add_argument('-i', dest='nmapFile', action='store', help='[use Nmap .xml file]')
     parser.add_argument('-o', dest='msfrc', action='store', help='[metasploit resource script]')
-    parser.add_argument('-nocache', action='store_true', help='[search Metasploit folder instead of using default-path.csv and port2Msf.csv]')
-    parser.add_argument('-findWeb', action='store_true', help='[find only HTTP/HTTPs exploits]')
-    parser.add_argument('-findPort', action='store_true', help='[find only port-based matched exploits]')
+    parser.add_argument('-nocache', action='store_true', help='[search Metasploit folder instead of using default-path.csv and port2Msf.csv (default=off]')
+    parser.add_argument('-findWeb', action='store_true', help='[find only HTTP/HTTPs exploits (default=on)]')
+    parser.add_argument('-findPort', action='store_true', help='[find only port-based matched exploits (default=on)]')
 
     if len(sys.argv)==1:
         parser.print_help()
@@ -233,6 +376,9 @@ if __name__== '__main__':
 
     options= parser.parse_args()
 
+    if options.nocache:
+	lookupAllPorts()
+	lookupURI(showModules=False)
     if options.findWeb:
 	findWeb=True
     else:
@@ -254,6 +400,7 @@ if __name__== '__main__':
 	outputFile="runMsf.rc"
 
     if len(contentList)>0:
+	contentList.append("exit -y")
 	f = open(outputFile, 'w')
 	for x in contentList:
 		f.write(x+"\n")
