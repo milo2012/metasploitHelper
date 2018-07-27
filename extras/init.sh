10cb905fd08bf0e4d407bea106d7a3f7605094c5
Keith Lee <Keith.lee2012@gmail.com>
2018-07-28 03:15:00 +0800

    temp add init.sh for docker container

--- extras/init.sh
+++ extras/init.sh
@@ -3,3 +3,4 @@ source /usr/local/rvm/scripts/rvm
 git config --global user.name "NAME HERE"
 git config --global user.email "email@example.com"
 /usr/share/metasploit-framework/msfupdate --git-branch master
+/bin/bash -c "cd /usr/local/bin/metasploitHelper && bash"
